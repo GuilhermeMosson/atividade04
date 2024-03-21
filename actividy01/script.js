47d80d065a517e7367f8a832e654b471e4135444
GuilhermeMosson <guimosson@gmail.com>
2024-03-21 19:09:04 -0300

    21/03 terminar at 4 e 5

--- actividy01/script.js
+++ actividy01/script.js
@@ -20,5 +20,45 @@ function confirmarAt02(){
         return;
     }
 
-    document.getElementById('msgInput').textContent = `O CÓDIGO: ${cod} IDADE: ${age} ALTURA: ${height} PESO: ${weight}`
+    document.getElementById('msgInput').textContent = `O CÓDIGO: ${cod} IDADE: ${age} anos; ALTURA: ${height}m PESO: ${weight} Kg`
+}
+
+function calcularSoma(){
+    var num01 = document.getElementById('num01').valueAsNumber;
+    var num02 = document.getElementById('num02').valueAsNumber;
+    var resulSoma = document.getElementById('resulSoma');
+
+    resulSoma.textContent = `Soma: ${num01 + num02}`
+}
+
+function calcularMult(){
+    var num01 = document.getElementById('num01').valueAsNumber;
+    var num02 = document.getElementById('num02').valueAsNumber;
+    var resulMult = document.getElementById('resulMult');
+
+    resulMult.textContent = `Multiplicação: ${num01 * num02}`
+}
+
+function contarAt04(){
+    var num = document.getElementById('numeroAt4').valueAsNumber;
+    var resul = document.getElementById('resAt4');
+
+
+    if(num < -99 || num > 99){
+        alert('INSIRA UM NÚMERO DE -99 À 99');
+        return;
+    }
+
+    for(var cont = num; cont <= 100; cont++);{
+        resul = resul + num + "-";
+        console.log(resul);
+    }
+}
+
+function calcularCircunferencia(){
+    var raio = document.getElementById('raio').valueAsNumber;
+    var circun = document.getElementById('cincunResu');
+    var resu = 2 * 3.14159 * raio; 
+
+    circun.textContent = `${resu}`;
 }
